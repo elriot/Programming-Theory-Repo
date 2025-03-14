364938e536dd142fb2282481643380d973105646
SKim <45351992+elriot@users.noreply.github.com>
2025-03-14 14:28:04 -0700

    GameManger Update Ball isMovable Status

--- Programming Theory Project/Assets/Scripts/BallController.cs
+++ Programming Theory Project/Assets/Scripts/BallController.cs
@@ -2,73 +2,65 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    private Rigidbody rb;
-    public float moveSpeed = 3f;
-    public bool isMovable = false;
-    // public int level; // 
+	private Rigidbody rb;
+	public float moveSpeed = 3f;
+	public bool isMovable = false;
+	// public int level; // 
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        rb.useGravity = false;
-    }
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.useGravity = false;
+	}
 
-    void Update()
-    {
-        if (isMovable && Input.GetKeyDown(KeyCode.Space))
-        {
-            Drop();
-        }
-    }
+	void Update()
+	{
+		if (isMovable && Input.GetKeyDown(KeyCode.Space))
+		{
+			Drop();
+		}
+	}
 
-    void FixedUpdate()
-    {
-        MoveBall();
-    }
+	void FixedUpdate()
+	{
+		MoveBall();
+	}
 
-    private void MoveBall()
-    {
-        if (!isMovable) return;
+	private void MoveBall()
+	{
+		if (!isMovable) return;
 
-        float horizontalInput = Input.GetAxis("Arrow Horizontal");
-        float verticalInput = Input.GetAxis("Arrow Vertical");
+		float horizontalInput = Input.GetAxis("Arrow Horizontal");
+		float verticalInput = Input.GetAxis("Arrow Vertical");
 
-        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized;
-        transform.Translate(movement * moveSpeed * Time.fixedDeltaTime);
-    }
+		Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized;
+		rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+	}
 
-    private void Drop()
-    {
-        rb.useGravity = true;
-        isMovable = false;
-    }
+	private void Drop()
+	{
+		rb.useGravity = true;
+		isMovable = false;
+	}
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        BallController otherBall = collision.gameObject.GetComponent<BallController>();
+	private void OnCollisionEnter(Collision collision)
+	{
+		BallController otherBall = collision.gameObject.GetComponent<BallController>();
 
-        if (otherBall != null && gameObject.tag == otherBall.tag && this.GetInstanceID() < otherBall.GetInstanceID())
-        {
-            Vector3 mergePosition = (transform.position + otherBall.transform.position) / 2;
-            int nextLevel = GetBallIndexByTag(gameObject.tag) + 1;
+		if (otherBall != null && gameObject.tag == otherBall.tag && this.GetInstanceID() < otherBall.GetInstanceID())
+		{
+			Vector3 mergePosition = (transform.position + otherBall.transform.position) / 2;
+			int nextLevel = GameManager.Instance.GetBallIndexByTag(gameObject.tag) + 1;
 
-            GameManager.Instance.SpawnMergeBall(mergePosition, nextLevel);
+			GameManager.Instance.SpawnMergeBall(mergePosition, nextLevel);
 
-            // 기존 볼 파괴
-            Destroy(gameObject);
-            Destroy(otherBall.gameObject);
-        }
-    }
-
-	private int GetBallIndexByTag(string tag)
-    {
-        if (string.IsNullOrEmpty(tag) || !tag.Contains("_"))
-        {
-            Debug.LogError($"Invalid tag format: {tag}");
-            return -1;
-        }
-
-        string indexString = tag.Substring(tag.LastIndexOf("_") + 1);
-        return int.TryParse(indexString, out int index) ? index : -1;
-    }
+			Destroy(gameObject);
+			Destroy(otherBall.gameObject);
+			GameManager.Instance.BallMovementCompleted();
+		}
+		else if(collision.gameObject.CompareTag("Floor"))
+		{
+			GameManager.Instance.BallMovementCompleted();
+		}
+	}
 }
--- Programming Theory Project/Assets/Scripts/GameManager.cs
+++ Programming Theory Project/Assets/Scripts/GameManager.cs
@@ -4,86 +4,101 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
-    public GameObject[] BallPrefabs;
-    [SerializeField] private int ballPrefabsIndexRange;
-    public int TotalPoint;
-    public Vector3 SpawnPos;
-    public TMP_Text ScoreText;
-    private BallController currentBall;
+	public static GameManager Instance;
+	public GameObject[] BallPrefabs;
+	[SerializeField] private int ballPrefabsIndexRange;
+	public int TotalPoint;
+	public Vector3 SpawnPos;
+	public TMP_Text ScoreText;
+	private BallController currentBall;
 
-    private int BallPrefabsLength => BallPrefabs.Length;
+	private int BallPrefabsLength => BallPrefabs.Length;
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-    }
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+	private void Start()
+	{
+		ballPrefabsIndexRange = 1;
+		UpdateScoreText();
+	}
 
-    private void Start()
-    {
-        ballPrefabsIndexRange = 0;
-        UpdateScoreText();
-    }
+	private void Update()
+	{
+		if (currentBall == null)
+		{
+			SpawnBall();
+		}
+	}
 
-    private void Update()
-    {
-        if (currentBall == null || !currentBall.isMovable)
-        {
-            currentBall = null;
-            SpawnBall();
-        }
-    }
+	private void SpawnBall()
+	{
+		if (ballPrefabsIndexRange < 0)
+		{
+			Debug.LogError("Ball Prefabs Index Range is zero or negative.");
+			return;
+		}
 
-    private void SpawnBall()
-    {
-        if (ballPrefabsIndexRange < 0)
-        {
-            Debug.LogError("Ball Prefabs Index Range is zero or negative.");
-            return;
-        }
-
-        int idx = UnityEngine.Random.Range(0, ballPrefabsIndexRange);
-        if (idx > ballPrefabsIndexRange)
-        {
-            Debug.LogError($"Out of Range - Ball Index : {idx}");
-            return;
-        }
+		int idx = UnityEngine.Random.Range(0, ballPrefabsIndexRange);
+		if (idx >= ballPrefabsIndexRange)
+		{
+			Debug.LogError($"Out of Range - Ball Index : {idx}");
+			return;
+		}
 
 		Debug.Log($"Spawn Ball Index : {idx}");
-        GameObject ballObj = Instantiate(BallPrefabs[idx], SpawnPos, Quaternion.identity);
-        currentBall = ballObj.GetComponent<BallController>();
+		GameObject ballObj = Instantiate(BallPrefabs[idx], SpawnPos, Quaternion.identity);
+		currentBall = ballObj.GetComponent<BallController>();
 		currentBall.isMovable = true;
-    }
+	}
 
 
-    public void UpdatePoint(int point)
-    {
-        TotalPoint += point;
-        UpdateScoreText();
-    }
+	public void AddPoints(int point)
+	{
+		TotalPoint += point;
+		UpdateScoreText();
+	}
 
-    private void UpdateScoreText()
-    {
-        ScoreText.text = $"Score : {TotalPoint}";
-    }
+	private void UpdateScoreText()
+	{
+		ScoreText.text = $"Score : {TotalPoint}";
+	}
 
-    public void BallMovementCompleted()
-    {
-        currentBall = null;
-    }
+	public void BallMovementCompleted()
+	{
+		currentBall = null;
+	}
 
-    public void SpawnMergeBall(Vector3 position, int level)
-    {
-        if (level < 0 || level >= BallPrefabs.Length)
-        {
-            Debug.LogError("Invalid level for spawning merge ball.");
-            return;
-        }
+	public void SpawnMergeBall(Vector3 position, int level)
+	{
+		if (level < 0 || level >= BallPrefabs.Length)
+		{
+			Debug.LogError("Invalid level for spawning merge ball.");
+			return;
+		}
 
-        GameObject newBall = Instantiate(BallPrefabs[level], position, Quaternion.identity);
-		newBall.GetComponent<BallController>().isMovable = false;
-    }
+		GameObject newBall = Instantiate(BallPrefabs[level], position, Quaternion.identity);
+		BallController ballController = newBall.GetComponent<BallController>();
+		ballController.isMovable = false;
+	}
+
+	public int GetBallIndexByTag(string tag)
+	{
+		if (string.IsNullOrEmpty(tag) || !tag.Contains("_"))
+		{
+			Debug.LogError($"Invalid tag format: {tag}");
+			return -1;
+		}
+
+		string indexString = tag.Substring(tag.LastIndexOf("_") + 1);
+		return int.TryParse(indexString, out int index) ? index : -1;
+	}
 }
