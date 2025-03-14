71a8864ab8432e5c2201fe59f565f145021229ab
SKim <45351992+elriot@users.noreply.github.com>
2025-03-13 20:41:21 -0700

    add isMovable, isDropped variables in BallController

--- Programming Theory Project/Assets/Scripts/BallController.cs
+++ Programming Theory Project/Assets/Scripts/BallController.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallController : MonoBehaviour
 {
 	private Rigidbody rb;
 	public float moveSpeed = 3f;
 	private GameObject cameraContainer;
+	public bool isMovable = true;
+	public bool isDropped = false;
 	
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-		cameraContainer = GameObject.Find("Camera");
+		rb.useGravity = false;
 
+		cameraContainer = GameObject.Find("Camera");
 		if (cameraContainer == null)
 		{
 			Debug.LogError("Camera container not found!");
@@ -19,31 +23,43 @@ public class BallController : MonoBehaviour
     }
 
     void FixedUpdate()
-    {
-		Debug.Log(cameraContainer.transform);
+	{
+		MoveBall();
 
-		float horizontalInput = Input.GetAxis("Arrow Horizontal"); // Left, Right Arrow
-        float verticalInput = Input.GetAxis("Arrow Vertical");     // Up, Down Arrow
-
-        Vector3 forward = cameraContainer.transform.forward;
-        Vector3 right = cameraContainer.transform.right;
-
-        // Only move horizontally (ignore Y axis)w
-        forward.y = 0f;
-        right.y = 0f;
-
-        forward.Normalize();
-        right.Normalize();
-
-        Vector3 movement = (right * horizontalInput + forward * verticalInput).normalized;
-
-        // Move the Rigidbody to avoid physics conflicts
-		transform.Translate(movement * moveSpeed * Time.fixedDeltaTime);
-        // rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-		
-        if(Input.GetKeyDown(KeyCode.Space))
+		if (isMovable && Input.GetKeyDown(KeyCode.Space))
 		{
-			rb.useGravity = true;
+			Drop();
 		}
-    }
+	}
+
+	private void MoveBall()
+	{
+		if(!isMovable) return;
+		
+		float horizontalInput = Input.GetAxis("Arrow Horizontal"); // Left, Right Arrow
+		float verticalInput = Input.GetAxis("Arrow Vertical");     // Up, Down Arrow
+
+		Vector3 forward = cameraContainer.transform.forward;
+		Vector3 right = cameraContainer.transform.right;
+
+		// Only move horizontally (ignore Y axis)w
+		forward.y = 0f;
+		right.y = 0f;
+
+		forward.Normalize();
+		right.Normalize();
+
+		Vector3 movement = (right * horizontalInput + forward * verticalInput).normalized;
+
+		// Move the Rigidbody to avoid physics conflicts
+		transform.Translate(movement * moveSpeed * Time.fixedDeltaTime);
+		// rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+	}
+
+	void Drop()
+	{
+		rb.useGravity = true;
+		isMovable = false;
+		isDropped = true;
+	}
 }
