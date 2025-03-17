5a0efee8a27b04ed546984e67869a15fc438b059
SKim <45351992+elriot@users.noreply.github.com>
2025-03-17 14:11:39 -0700

    Add SoundManager
    but can not hear sounds

---
+++ Programming Theory Project/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+	private AudioSource audioSource;
+
+	public AudioClip DropBallSound;
+	public AudioClip MergeBallSound;
+	public float EffectVolume;
+	public float BGMVolume;
+
+	void Awake()
+	{
+		if(Instance == null)
+		{
+			Instance = this;
+		}
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    public void PlayDropSound()
+    {
+        PlaySound(DropBallSound);
+		audioSource.PlayOneShot(DropBallSound, 1.0f);
+    }
+
+    public void PlayMergeSound()
+    {
+        PlaySound(MergeBallSound);
+    }
+
+    // 일반적인 사운드 재생 함수
+    private void PlaySound(AudioClip clip)
+    {
+        if (clip != null)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+        else
+        {
+            Debug.LogWarning("Attempted to play a null audio clip.");
+        }
+    }
+}
